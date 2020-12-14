1a96c2f2f43c796bef32fcd305f00df55c9afbf9
Arshad-97 <agmarshad97@gmail.com>
2020-12-14 15:58:44 +0530

    Step 1 : Successfully created POST  API for store the post

--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const posts = require('../models/posts');
 const router = express.Router();
 const Post = require('../models/posts');
 
@@ -9,6 +10,18 @@ router.get("/", (req, res) => {
         .catch(err => res.status(400).res.json(`error: ${err}`));
 });
 
+// REQUEST ADD NEW POST
+router.post("/add", (req, res) => {
+    const newPost = posts({
+        title: req.body.title,
+        post: req.body.post,
+        authorname: req.body.authorname
+    });
 
+    newPost
+        .save()
+        .then(() => res.json("Post successfully added !"))
+        .catch(err => res.status(400).json(`error: ${err}`));
+});
 
 module.exports = router;
